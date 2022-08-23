ccd17676b1acd82275cc343b12bcb88790198b50
BarbysBuc <110196196+BarbysBuc@users.noreply.github.com>
2022-08-23 09:54:42 -0300

    agregando funcionalidad para verificar solo minusculas

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var copiarBtn = document.querySelector(".copiar-boton");
 
 encriptarBtn.onclick = encriptar;
 desencriptarBtn.onclick = desencriptar;
-copiarBtn.onclick= copiar;
+copiarBtn.onclick = copiar;
 
 function visualAdecuada() {
   if (visualSalida1.style.display != "none") {
@@ -19,55 +19,66 @@ function visualAdecuada() {
 function encriptar() {
   var texto = document.querySelector("#texto").value;
   var textoFinal = "";
+  const minusculas = /(?:[a-z])/.test(texto);
+  console.log("minusculas", minusculas);
+  const mayusculas = !/(?:[A-Z])/.test(texto);
+  console.log("mayusculas", mayusculas);
+  const numeros = !/(?:\d)/.test(texto);
+  console.log("numeros", numeros);
+  const especiales = !/[^A-Za-z\d]/.test(texto);
+  console.log("especiales", especiales);
+  if (minusculas && mayusculas && numeros && especiales) {
+    for (var i = 0; i < texto.length; i++) {
+      if (texto[i] == "a") {
+        textoFinal = textoFinal + "ai";
+      } else if (texto[i] == "e") {
+        textoFinal = textoFinal + "enter";
+      } else if (texto[i] == "i") {
+        textoFinal = textoFinal + "imes";
+      } else if (texto[i] == "o") {
+        textoFinal = textoFinal + "ober";
+      } else if (texto[i] == "u") {
+        textoFinal = textoFinal + "ufat";
+      } else {
+        textoFinal = textoFinal + texto[i];
+      }
+    }
+    visualAdecuada();
+    return (textoResultado.innerHTML = textoFinal);
+  } else {
+    alert("Solo letras minúsculas sin acentos");
+  }
+}
+
+function desencriptar() {
+  var texto = document.querySelector("#texto").value;
+  var textoFinal = "";
   for (var i = 0; i < texto.length; i++) {
     if (texto[i] == "a") {
-      textoFinal = textoFinal + "ai";
+      textoFinal = textoFinal + "a";
+      i = i + 1;
     } else if (texto[i] == "e") {
-      textoFinal = textoFinal + "enter";
+      textoFinal = textoFinal + "e";
+      i = i + 4;
     } else if (texto[i] == "i") {
-      textoFinal = textoFinal + "imes";
+      textoFinal = textoFinal + "i";
+      i = i + 3;
     } else if (texto[i] == "o") {
-      textoFinal = textoFinal + "ober";
+      textoFinal = textoFinal + "o";
+      i = i + 3;
     } else if (texto[i] == "u") {
-      textoFinal = textoFinal + "ufat";
+      textoFinal = textoFinal + "u";
+      i = i + 3;
     } else {
       textoFinal = textoFinal + texto[i];
     }
   }
-
-  visualAdecuada();
   return (textoResultado.innerHTML = textoFinal);
 }
 
-function desencriptar(){
-    var texto = document.querySelector("#texto").value;
-    var textoFinal = "";
-    for (var i = 0; i < texto.length; i++) {
-        if (texto[i] == "a") {
-          textoFinal = textoFinal + "a";
-          i = i + 1;
-        } else if (texto[i] == "e") {
-          textoFinal = textoFinal + "e";
-          i = i + 4;
-        } else if (texto[i] == "i") {
-          textoFinal = textoFinal + "i";
-          i = i + 3;
-        } else if (texto[i] == "o") {
-          textoFinal = textoFinal + "o";
-          i = i + 3;
-        } else if (texto[i] == "u") {
-          textoFinal = textoFinal + "u";
-          i = i + 3;
-        } else {
-          textoFinal = textoFinal + texto[i];
-        }
-    }
-    return (textoResultado.innerHTML = textoFinal);
-}
-
-function copiar(){
-    let textoCopiado = textoResultado;
-    textoCopiado.select();
-    document.execCommand("copy");
-    textoResultado.innerHTML = "";
+function copiar() {
+  let textoCopiado = textoResultado;
+  textoCopiado.select();
+  document.execCommand("copy");
+  textoResultado.innerHTML = "";
 }
